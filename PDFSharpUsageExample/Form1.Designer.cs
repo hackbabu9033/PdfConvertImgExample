28080e303647c8cc4000f4e790d4bd04195d9004
hank <hank@wishingsoft.com>
2022-03-17 13:31:00 +0800

    用兩個free lib做pdf轉img的測試 pdfiumview freespire.pdf

---
+++ PDFSharpUsageExample/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace PDFSharpUsageExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Btn_selectFile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(291, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Btn_selectFile
+            // 
+            this.Btn_selectFile.Location = new System.Drawing.Point(415, 94);
+            this.Btn_selectFile.Name = "Btn_selectFile";
+            this.Btn_selectFile.Size = new System.Drawing.Size(75, 23);
+            this.Btn_selectFile.TabIndex = 1;
+            this.Btn_selectFile.Text = "Select File";
+            this.Btn_selectFile.UseVisualStyleBackColor = true;
+            this.Btn_selectFile.Click += new System.EventHandler(this.Btn_selectFile_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Btn_selectFile);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Btn_selectFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ PDFSharpUsageExample/Form1.cs
@@ -0,0 +1,102 @@
+﻿using PdfiumViewer;
+using System;
+using System.ComponentModel;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace PDFSharpUsageExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            
+        }
+
+
+        private void ProcessPDFFileToImage(string fileName)
+        {
+            #region spire.pdf lib
+            //PdfDocument doc = new PdfDocument();
+            //doc.LoadFromFile(fileName);
+            //for (int i = 0; i < doc.Pages.Count; i++)
+            //{
+            //    Image bmp = doc.SaveAsImage(i);
+            //    Image emf = doc.SaveAsImage(i, Spire.Pdf.Graphics.PdfImageType.Metafile);
+            //    Image zoomImg = new Bitmap((int)(emf.Size.Width * 2), (int)(emf.Size.Height * 2));
+            //    using (Graphics g = Graphics.FromImage(zoomImg))
+            //    {
+            //        g.ScaleTransform(2.0f, 2.0f);
+            //        g.DrawImage(emf, new Rectangle(new Point(0, 0), emf.Size), new Rectangle(new Point(0, 0), emf.Size), GraphicsUnit.Pixel);
+            //    }
+            //    var bmpFileName = string.Format("convertToBmp{0}.bmp", i);
+            //    bmp.Save(bmpFileName, ImageFormat.Bmp);
+            //    System.Diagnostics.Process.Start(bmpFileName);
+            //    var emfFileName = string.Format("convertToEmf{0}.bmp", i);
+            //    emf.Save(emfFileName, ImageFormat.Png);
+            //    System.Diagnostics.Process.Start(emfFileName);
+            //    var zoomImgFileName = string.Format("convertToZoom{0}.bmp", i);
+            //    zoomImg.Save(zoomImgFileName, ImageFormat.Png);
+            //    System.Diagnostics.Process.Start(zoomImgFileName);
+            //}
+            #endregion
+
+            #region use pdfiumviewer
+            using (var doc = PdfDocument.Load(fileName)) // C# Read PDF File
+            {
+                for (int i = 0; i < doc.PageCount; i++)
+                {
+                    var dpi = 300;
+
+                    using (var image = doc.Render(i, dpi, dpi, PdfRenderFlags.CorrectFromDpi))
+                    {
+                        var encoder = ImageCodecInfo.GetImageEncoders()
+                            .First(c => c.FormatID == ImageFormat.Jpeg.Guid);
+                        var encParams = new EncoderParameters(1);
+                        encParams.Param[0] = new EncoderParameter(
+                            System.Drawing.Imaging.Encoder.Quality, 100L);
+
+                        image.Save(@"output_withEncoder_" + i + ".jpg", encoder, encParams);
+                        image.Save(@"output_" + i + ".jpg", ImageFormat.Jpeg);
+                        //image.Save(@"output_" + i + ".jpg", encoder, encParams);
+                        //image.Save(@"output_" + i + ".jpg", encoder, encParams);
+                    }
+                }
+            }
+            #endregion
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_selectFile_Click(object sender, EventArgs e)
+        {
+            // Show the dialog that allows user to select a file, the 
+            // call will result a value from the DialogResult enum
+            // when the dialog is dismissed.
+            DialogResult result = this.openFileDialog1.ShowDialog();
+            // if a file is selected
+            if (result == DialogResult.OK)
+            {
+                // Set the selected file URL to the textbox
+                this.textBox1.Text = this.openFileDialog1.FileName;
+                ProcessPDFFileToImage(this.openFileDialog1.FileName);
+            }
+        }
+    }
+}
